3163bc1673b128aa7a9d9b6958c20e6da2424afc
ZamniProg <artemopbgsg@gmail.com>
2024-11-20 17:16:35 +0300

    Updated class FullConnectedLayer

--- MAIN.py
+++ MAIN.py
@@ -7,6 +7,10 @@ import numpy as np
 classes = {"daisy": 0, "dandelion": 1, "roses": 2, "sunflowers": 3, "tulips": 4}
 
 
+def relu(x):
+    return np.max(0, x)
+
+
 def load_data(path):
     learn_data = []
     train_data = []
@@ -30,14 +34,36 @@ def load_data(path):
 
 
 class FullConnectedLayer:
-    def __init__(self, input_size, output_size):
-        self.weights = np.random.randn(input_size)
+    """Полносвязный слой для нелинейного преобразования всех предыдущих входных данных"""
+    def __init__(self, input_size, output_size, activation=relu):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.weights = np.random.randn(input_size, output_size) * 0.01
+        self.biases = np.zeros((1, output_size))
+        self.activation = activation
+        self.input_data = None
 
-    def forward(self):
-        pass
+    def forward(self, input_data):
+        h_s, w_s, channels = input_data.shape
 
-    def calculate_grads(self):
-        pass
+        input_x = input_data.reshape(input_data.shape[0], -1)
+        self.input_data = input_x
+
+        z = np.dot(input_x, self.weights) + self.biases
+
+        output = self.activation(z) if self.activation else z  # функция активации
+        return output
+
+    def calculate_grads(self, d_out, learning_rate=0.01):
+        d_weights = np.dot(self.input_data.T, d_out)
+        d_biases = np.sum(d_out, axis=0, keepdims=True)
+
+        d_input = np.dot(d_out, self.weights.T)
+
+        self.weights -= learning_rate * d_weights
+        self.biases -= learning_rate * d_biases
+
+        return d_input
 
 
 class MaxPoolLayer:
@@ -45,9 +71,12 @@ class MaxPoolLayer:
     def __init__(self, pool_size=2, stride=2):
         self.pool_size = pool_size
         self.stride = stride
+        self.input_data = None
 
     def forward(self, input_data):
         h, w, c = input_data.shape
+
+        self.input_data = input_data
 
         h_out = (h - self.pool_size) // self.stride + 1
         w_out = (w - self.pool_size) // self.stride + 1
@@ -67,11 +96,11 @@ class MaxPoolLayer:
 
         return out
 
-    def backward_prop(self, d_out, input_data):
-        h_s, w_s, channels = input_data.shape
+    def backward_prop(self, d_out):
+        h_s, w_s, channels = self.input_data.shape
         h_e, w_e, channels = d_out.shape
 
-        d_input = np.zeros_like(input_data)
+        d_input = np.zeros_like(self.input_data)
 
         for c in range(channels):
             for h in range(h_e):
@@ -82,7 +111,7 @@ class MaxPoolLayer:
                     w_start = w * self.stride
                     w_end = w_start + self.pool_size
 
-                    region = input_data[h_start:h_end, w_start:w_end, c]
+                    region = self.input_data[h_start:h_end, w_start:w_end, c]
                     max_val = np.max(region)
 
                     d_input[h_start:h_end, w_start:w_end, c] += (region == max_val) * d_out[h, w, c]
@@ -92,7 +121,7 @@ class MaxPoolLayer:
 
 class ConvolutionLayer:     # maybe ready
     """Слой для свертки изображения"""
-    def __init__(self, filter_size, num_filters, num_channels, stride=2, learning_rate=0.01):
+    def __init__(self, filter_size, num_filters, num_channels, stride=2, learning_rate=0.01, activation=relu):
         self.num_filters = num_filters
         self.num_channels = num_channels
         self.stride = stride
@@ -100,9 +129,12 @@ class ConvolutionLayer:     # maybe ready
         self.filters = np.random.randn(filter_size, filter_size, num_channels, num_filters) * 0.01  # rework
         self.biases = np.zeros((num_filters, ))
         self.learning_rate = learning_rate
+        self.activation = activation
+        self.image = None
 
     def forward(self, image):
         h, w, _, channels = image.shape
+        self.image = image
         h_out = (h - self.filter_size) // self.stride + 1
         w_out = (w - self.filter_size) // self.stride + 1
 
@@ -120,15 +152,17 @@ class ConvolutionLayer:     # maybe ready
 
                     out[k1, k2, f] = np.sum(region * self.filters[:, :, :, f]) + self.biases[f]
 
+        out = self.activation(out) if self.activation else out
+
         return out
 
-    def backward_prop(self, image, grad_out):
-        h_image, w_image, c_image = image.shape
+    def backward_prop(self, grad_out):
+        h_image, w_image, c_image = self.image.shape
         h_out, w_out, num_filters = grad_out.shape
 
         d_filters = np.zeros_like(self.filters)
         d_biases = np.zeros_like(self.biases)
-        d_input = np.zeros_like(image)
+        d_input = np.zeros_like(self.image)
 
         for f in range(self.num_filters):
             for k1 in range(h_out):
@@ -138,7 +172,7 @@ class ConvolutionLayer:     # maybe ready
                     w_start = k2 * self.stride
                     w_end = w_start + self.filter_size
 
-                    region = image[h_start:h_end, w_start:w_end, :]
+                    region = self.image[h_start:h_end, w_start:w_end, :]
 
                     d_filters[:, :, :, f] += region * grad_out[k1, k2, f]
 
@@ -146,13 +180,10 @@ class ConvolutionLayer:     # maybe ready
 
             d_biases[f] += np.sum(grad_out[:, :, f])
 
-        self.update_params(d_filters, d_biases)
-
-        return d_input, d_filters, d_biases
-
-    def update_params(self, d_filters, d_biases):
         self.filters -= self.learning_rate * d_filters
         self.biases -= self.learning_rate * d_biases
+
+        return d_input, d_filters, d_biases
 
 
 class NeuralNetwork:
